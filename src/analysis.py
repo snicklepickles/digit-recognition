f7f00473889d34caa83ede0b76df77e940acbc59
Phoebe Zhang <zphoebe@outlook.com>
2022-08-29 23:34:28 +1000

    Refactored code

--- src/analysis.py
+++ src/analysis.py
@@ -3,22 +3,15 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-"""
-Loads the MNIST dataset
-X is a NumPy array of grayscale image data
-y is a NumPy array of digit labels (integers in range 0-9)
-"""
+# x is an array of grayscale image data
+# y is an array of digit labels (integers in range 0-9)
+(x, y) = tf.keras.datasets.mnist.load_data()[0]
 
-(X, y) = tf.keras.datasets.mnist.load_data()[0]
-
-# Reshapes x_train to a 2D array of 6000 rows (images) and 784 columns (28 x 28 pixels per image)
-features = np.reshape(X, (60000, 784))[:6000, :]
-
-# Reshapes y_train to a 1D array of 6000 rows (labels)
-targets = np.reshape(y, (60000,))[:6000]
+# Reshapes x to a 2D array (number of images x pixels per image)
+X = np.reshape(x, (60000, 28 * 28))
 
 # Splits the data into training and test sets
-x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size=0.3, random_state=40)
+x_train, x_test, y_train, y_test = train_test_split(X[:6000], y[:6000], test_size=0.3, random_state=40)
 
 print(f"x_train shape: {x_train.shape}")
 print(f"x_test shape: {x_test.shape}")
@@ -26,8 +19,8 @@ print(f"y_train shape: {y_train.shape}")
 print(f"y_test shape: {y_test.shape}")
 
 print("Proportion of samples per class in train set:")
-s = pd.Series(y_train)
-print(round(s.value_counts(normalize=True), 2))
+ds = pd.Series(y_train)
+print(round(ds.value_counts(normalize=True), 2))
 
 # print("Classes: " + str(np.unique(targets)))
 # print("Features' shape " + str(features.shape))
