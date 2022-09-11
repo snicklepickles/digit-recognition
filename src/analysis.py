a7a0fd26ba2823bde2e5ff9fda20455b476cd8d9
Phoebe Zhang <zphoebe@outlook.com>
2022-09-11 20:09:35 +1000

    Trained and evaluated various sklearn models

--- src/analysis.py
+++ src/analysis.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
-import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
 
 # x is an array of grayscale image data
 # y is an array of digit labels (integers in range 0-9)
@@ -13,16 +17,22 @@ X = np.reshape(x, (60000, 28 * 28))
 # Splits the data into training and test sets
 x_train, x_test, y_train, y_test = train_test_split(X[:6000], y[:6000], test_size=0.3, random_state=40)
 
-print(f"x_train shape: {x_train.shape}")
-print(f"x_test shape: {x_test.shape}")
-print(f"y_train shape: {y_train.shape}")
-print(f"y_test shape: {y_test.shape}")
 
-print("Proportion of samples per class in train set:")
-ds = pd.Series(y_train)
-print(round(ds.value_counts(normalize=True), 2))
+# Trains a classifier and returns its precision score
+def fit_predict_eval(model, features_train, features_test, target_train, target_test):
+    model.fit(features_train, target_train)
+    y_pred = model.predict(features_test)
+    score = precision_score(target_test, y_pred, average='macro')
+    print(f'Model: {model}\nAccuracy: {score.round(2)}\n')
+    return score
 
-# print("Classes: " + str(np.unique(targets)))
-# print("Features' shape " + str(features.shape))
-# print("Targets' shape " + str(targets.shape))
-# print("min: " + str(np.min(features)), "max: " + str(np.max(features)))
+
+# Trains four different classifiers and prints the one with the highest precision score
+highest_score = 0
+best_model = None
+for classifier in (KNeighborsClassifier(), DecisionTreeClassifier(random_state=40), LogisticRegression(random_state=40), RandomForestClassifier(random_state=40)):
+    score = fit_predict_eval(classifier, x_train, x_test, y_train, y_test)
+    if score > highest_score:
+        highest_score = score
+        best_model = type(classifier).__name__
+print(f'The answer to the question: {best_model} - {highest_score.round(3)}')
