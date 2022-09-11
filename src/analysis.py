863bf834528f142a301d4dceb61dfb25c6f9d594
Phoebe Zhang <zphoebe@outlook.com>
2022-09-11 20:34:18 +1000

    Normalised training data

--- src/analysis.py
+++ src/analysis.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
+from sklearn.preprocessing import Normalizer
 
 # x is an array of grayscale image data
 # y is an array of digit labels (integers in range 0-9)
@@ -16,6 +17,10 @@ X = np.reshape(x, (60000, 28 * 28))
 
 # Splits the data into training and test sets
 x_train, x_test, y_train, y_test = train_test_split(X[:6000], y[:6000], test_size=0.3, random_state=40)
+
+# Normalises the training data
+x_train = Normalizer().fit_transform(x_train)
+x_test = Normalizer().fit_transform(x_test)
 
 
 # Trains a classifier and returns its precision score
@@ -28,11 +33,9 @@ def fit_predict_eval(model, features_train, features_test, target_train, target_
 
 
 # Trains four different classifiers and prints the one with the highest precision score
-highest_score = 0
-best_model = None
+scores = []
 for classifier in (KNeighborsClassifier(), DecisionTreeClassifier(random_state=40), LogisticRegression(random_state=40), RandomForestClassifier(random_state=40)):
     score = fit_predict_eval(classifier, x_train, x_test, y_train, y_test)
-    if score > highest_score:
-        highest_score = score
-        best_model = type(classifier).__name__
-print(f'The answer to the question: {best_model} - {highest_score.round(3)}')
+    scores.append((type(classifier).__name__, score))
+scores.sort(key=lambda x: x[1], reverse=True)
+print(f'Two best-scoring models: {scores[0][0]}: {scores[0][1].round(3)}, {scores[1][0]}: {scores[1][1].round(3)}')
